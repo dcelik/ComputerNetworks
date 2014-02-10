1bab6583d7ac766785e42cac1f9456e349311d8b
unknown <nickfrancisci@gmail.com>
2014-02-10 17:24:45 -0500

    Continued to improve getPulseWidth for improve statistical accuracy. This iteration adds the ability to split a bimodal distribution based on the mean, and the ability to clean the data for single flipped bits.

--- getPulseWidth.py
+++ getPulseWidth.py
@@ -18,18 +18,21 @@ def getPulseWidth(data, verbose=False, header=True):
 	header = True if the data being passed in the known_sample_header 
 	Returns: the determined pulse width of the transmission
 	"""
-
 	#Get a list of the three most common lengths of zeros,
 	#and the two most common lengths of ones
-	pulseLengthZeros,pulseLengthOnes = getOccuranceFrequencies([int(i) for i in data]);
+	if verbose: print("Initial data string: " + str(data));
+	data = cleanData([int(i) for i in data]);
+	if verbose: print("Cleaned data string: " + str(data));
+	pulseLengthZeros,pulseLengthOnes = getOccuranceFrequencies(data);
+	if verbose: print("Modes from zeros list: " + str(pulseLengthZeros));
+	if verbose: print("Modes from ones list: " + str(pulseLengthOnes));
 	
 	#Compile a list of the potential pulse widths based on the known relations of morse code
 	potentialWidths = [];
-	print(pulseLengthZeros);
 	
 	if(len(pulseLengthZeros)>0):potentialWidths.append(pulseLengthZeros[0]);	#Based on symbol spaces
 	if(len(pulseLengthZeros)>1):potentialWidths.append(pulseLengthZeros[1]//3);	#Based on character spaces
-	#if(len(pulseLengthZeros)>2):potentialWidths.append(pulseLengthZeros[2]//7);	#Based on word spaces
+	#if(len(pulseLengthZeros)>2):potentialWidths.append(pulseLengthZeros[2]//7);	#Based on word spaces OMITTED BECAUSE THIS CAUSES DATA ISSUES
 	if(len(pulseLengthOnes)>0): potentialWidths.append(pulseLengthOnes[0]);	        #Based on dot length
 	if(len(pulseLengthOnes)>1): potentialWidths.append(pulseLengthOnes[1]//3);	#Based on dash length
 	
@@ -37,14 +40,27 @@ def getPulseWidth(data, verbose=False, header=True):
 
 	if(verbose):
 		print("Pulse width determined to be " +str(pulse_width) +" with a  data goodness of " + str(data_goodness) +
-		      " (measured from 0 to 1) which is based on " + str(len(pulseLengthOnes)+len(pulseLengthZeros)) + " evaluations out of a maximum 5.");
+		      " (measured from 0 to 1) which is based on " + str(len(pulseLengthOnes)+len(pulseLengthZeros)) + " evaluations out of a maximum 4.");
 		print("Note: data goodness is simply a measure of the uniformity of the most common  binary occurances " +
 		      "in the code for the purposes of obtaining a pulse width and should not be taken to mean that "+
 		      "the message has transmitted without errors.")
 		if(data_goodness<=.5): print("Recorded pulses are irregular - consider requesting repeat of message.");
-	
+
 	return pulse_width;
 
+def cleanData(data):
+	"""
+	Requires list of ints. Cleans the data by averaging and rounding every three consecutive elements
+	"""
+	returnData = [data[0]];
+	i = 1;
+	while i<len(data)-1:
+		returnData.append((data[i-1]+data[i]+data[i+1])//3);
+		i+=1;
+
+	returnData.append(data[-1]);
+
+	return returnData;
 
 def getOccuranceFrequencies(data):
 	"""
@@ -78,14 +94,39 @@ def getOccuranceFrequencies(data):
 	if pulseIsOnes: pulsesOnes.append(pulseLength);
 	else: pulsesZeros.append(pulseLength);
 
-
+	#Compute mean of each list
+	#Then splits each list into two based on groupings above and below the mean
 	#Extract modes from lists
-	pulseLengthZeros = Counter(pulsesZeros).most_common(2);
-	pulseLengthOnes = Counter(pulsesOnes).most_common(2)
-	print("Modal distribution of ones: " +str(pulseLengthZeros));
-	print("Modal distribution of zeros: " +str(pulseLengthOnes));
+	if not len(pulsesZeros)==0:
+		pulsesZeros.sort();
+		mean_zeros = sum(pulsesZeros)//len(pulsesZeros);
+		lowZerosList = pulsesZeros[:mean_zeros];
+		highZerosList = pulsesZeros[mean_zeros+1:];
+		low_zero = Counter(lowZerosList).most_common(1);
+		high_zero = Counter(highZerosList).most_common(1);
+		pulseLengthZeros = low_zero + high_zero;
+		print(pulseLengthZeros)
+		pulseLengthZeros = sorted([int(tup[0]) for tup in pulseLengthZeros])
+		print(pulseLengthZeros)
+	else:
+		pulseLengthZeros = [];
+		
+	if not len(pulsesOnes)==0:
+		pulsesOnes.sort();
+		mean_ones = sum(pulsesOnes)//len(pulsesOnes);
+		lowOnesList = pulsesOnes[:mean_ones];
+		highOnesList = pulsesOnes[mean_ones+1:];
+		low_one = Counter(lowOnesList).most_common(1);
+		high_one = Counter(highOnesList).most_common(1);
+		pulseLengthOnes = low_one + high_one;
+		print(pulseLengthOnes)
+		pulseLengthOnes = sorted([int(tup[0]) for tup in pulseLengthOnes])
+		print(pulseLengthOnes)
+	else:
+		pulseLengthOnes = [];
 
-	return sorted([int(key) for key in dict(pulseLengthZeros).keys()]),sorted([int(key) for key in dict(pulseLengthOnes).keys()]);
+	return pulseLengthZeros,pulseLengthOnes;
+
 
 
 def findPulseWidth(data):
