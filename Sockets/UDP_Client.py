6ed58a433b680d1b435dde12eb964448120cf9a1
unknown <nickfrancisci@gmail.com>
2014-02-20 18:09:31 -0500

    Continued to Patch Server Bugs, added admin

--- Sockets/UDP_Client.py
+++ Sockets/UDP_Client.py
@@ -6,7 +6,7 @@ class UDP_Client(object):
     """ Computer Networks Chapter 4: Sockets.  UDP Client example. """ 
     
     
-    def __init__(self,Server_Address=("192.168.49.117",5280)):
+    def __init__(self,Server_Address=("10.7.24.121",5280)):
 
         socket, AF_INET, SOCK_DGRAM = CN_Sockets.socket, CN_Sockets.AF_INET, CN_Sockets.SOCK_DGRAM
 
