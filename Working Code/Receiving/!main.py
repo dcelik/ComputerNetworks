a34691fc54ad4d919a7f7e7e941c3a4e48daaa5c
Ian Hill <ian.hill@students.olin.edu>
2014-02-24 16:44:57 -0500

    created packet catching function

--- Working Code/Receiving/!main.py
+++ Working Code/Receiving/!main.py
@@ -1,9 +1,13 @@
-from catchPacket import catchPacket
+from catchPacket import *
 
 def main():
     print("Receiver Online...")
+    z = takeMeasurement()
+    initialPacket = [z,1]
     while True:
-        print catchPacket()
+        currentPacket = catchPacket(initialPacket)
+        initialPacket = [not currentPacket[0],2]
+        print(currentPacket),
 
 if __name__ == "__main__":
     main()
