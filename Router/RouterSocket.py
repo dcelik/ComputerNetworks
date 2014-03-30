90003dca30ec1e2e21942c817983efc08deb213d
themythicaldrago <ryan.louie@students.olin.edu>
2014-03-30 11:22:55 -0400

    Updated RouterSocket, and proper interfaces with it

--- Router/RouterSocket.py
+++ Router/RouterSocket.py
@@ -1,81 +1,63 @@
 from CustomSockets import CustomSocket
 
 class RouterSockets(CustomSocket):
-  	def __init__(self,Router_Address):
-    # initialize the super class
-    # figure our the macDict problem
-  
-  	def sendto(self,msg,address):
+  	def __init__(self,family = 2, protocol = 2, router_mac="T",verbose=False,debug=True):
+                super(RouterSockets, self).__init__(family,protocol,router_mac,verbose,debug)
+                self.macDict = {"II":"I",
+                                "IR":"R",
+                                "IN":"N",
+                                "ID":"D",
+                                }
+  	def sendto(self,msg,destination_address, source_address):
   		""" Router Should "forward" a message on, meaning it should not add it's
-    	own ip/udp address to the header
-    	"""
-    	address = self.pubIPToMorse(address);
-        
-        if not self.validIPAndPort:
-            print("Error: Invalid IP and port or socket has not been bound with an IP and port: message not sent!");
-            return;
+                own ip/udp address to the header
 
-        to_ip_addr = address[0];
-        to_port = address[1];
-        msg = msg.decode("utf-8"); #Convert from bytearray to a string for ease of operation
+                Thus, a source_address of the original sender is needed
+                """
+                # NOTE: Staying in Morse (II) vs pubIP (0.0.73.73)
 
-        # Assemble UDP package
-        udp_package = to_port + self.my_port + msg;
+                if not self.validIPAndPort:
+                        print("Error: Invalid IP and port or socket has not been bound with an IP and port: message not sent!");
+                        return;
 
-        # Assemble IP package
-        ip_header = to_ip_addr + self.my_ip_addr + self.protocol_identifier + t.base36encode(len(udp_package));
-        ip_package = ip_header + udp_package;
+                to_ip_addr, to_port = destination_address
+                from_ip_addr, from_port = source_address
+                msg = msg.decode("utf-8"); #Convert from bytearray to a string for ease of operation
 
-        # Assemble MAC package
-            # First check to see if the MAC of the recieving IP is known, if not address message to router
-        if to_ip_addr in self.macDict: mac_to = self.macDict[to_ip_addr];
-        else: mac_to = self.macDict['router_mac'];   # This only works if you're not the router...
-            # Then assemble the remainder of the MAC package
-        mac_from = self.my_mac;
-        # Send the message
-        t.sendMessage(mac_to,mac_from,ip_package);
+                # Assemble UDP package
+                udp_package = to_port + from_port + msg;
+
+                # Assemble IP package
+                ip_header = to_ip_addr + from_ip_addr + self.protocol_identifier + t.base36encode(len(udp_package));
+                ip_package = ip_header + udp_package;
+
+                # Assemble MAC package
+                mac_to = self.macDict[to_ip_addr]
+                mac_from = self.macDict[from_ip_addr]
+                
+                # Send the message
+                t.sendMessage(mac_to,mac_from,ip_package);
   
   	def recvfrom(self, buflen):
-    	# copy code from routerRecvFrom (below)
-    	data = baseRecv(buflen);
-		if data is not None:
-			message = data[0]; 
+                # copy code from routerRecvFrom (below)
+                data = baseRecv(buflen);
+                        if data is not None:
+                                message = data[0]; 
 
-			mac_header = data[1];
-			ip_header = data[2];
-			udp_header = data[3];
+                                mac_header = data[1];
+                                ip_header = data[2];
+                                udp_header = data[3];
 
-			ip_to = ip_header[0];
-			ip_from = ip_header[1];
-			udp_to = udp_header[0];
-			udp_from = udp_header[1];
+                                ip_to = ip_header[0];
+                                ip_from = ip_header[1];
+                                udp_to = udp_header[0];
+                                udp_from = udp_header[1];
 
-			bytearray_msg = bytearray(message, encoding="utf-8")
+                                bytearray_msg = bytearray(message, encoding="utf-8")
 
-			return bytearray_msg, (ip_from,udp_from) , (ip_to,udp_to), ip_header, udp_header; 
-		else: return None;
+                                return bytearray_msg, (ip_from,udp_from) , (ip_to,udp_to), ip_header, udp_header; 
+                        else: return None;
   
   
-  	def routerRecvFrom(self, buflen):
-		""" 
-		Uses the same code as the recieve function, but returns additional data for use
-		by the router.
-
-		"""
-
-		data = baseRecv(buflen);
-		if data is not None:
-			message = data[0];
-			mac_header = data[1];
-			ip_header = data[2];
-			udp_header = data[3];
-
-			ip_to = ip_header[0];
-			ip_from = ip_header[1];
-			udp_to = udp_header[0];
-			udp_from = udp_header[1];
-
-			return message, (ip_from,udp_from) ,(ip_to,udp_to); 
-		else: return None;
             
   
