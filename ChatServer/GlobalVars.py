56c022d87ccfb344feefe737a4a71784b2567570
Ian Hill <ian.hill@students.olin.edu>
2014-05-06 20:21:33 -0400

    File transfer

---
+++ ChatServer/GlobalVars.py
@@ -0,0 +1,16 @@
+#--------Commands--------#
+command_symbol = "/";
+
+#--------Server Lists--------#
+Users = {};
+IPs = {};
+ServerLog = [];
+BannedIPs=[];
+
+#--------Misc Vars--------#
+default_client_port = "ERROR: PORT NOT ASSIGNED";
+server_port = "69";
+server_ip = "0.0.73.78";
+client_buffer_size = 1024;
+message_per_timeout_limit = 5;
+admin_pw = "nick";
